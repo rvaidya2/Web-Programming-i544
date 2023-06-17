6b1e8b04653b600f6113fadc08a1befac6244597
Rujuta Vivek Vaidya <rvaidya2@binghamton.edu>
2023-06-17 11:50:06 -0400

    3 testcases passed

--- submit/prj1-sol/src/lib/spreadsheet.ts
+++ submit/prj1-sol/src/lib/spreadsheet.ts
@@ -1,3 +1,4 @@
+import { stringify } from 'querystring';
 import {default as parse, CellRef, Ast } from './expr-parser.js';
 
 import { Result, okResult, errResult } from 'cs544-js-utils';
@@ -11,12 +12,36 @@ export default async function makeSpreadsheet(name: string) :
 
 type Updates = { [cellId: string]: number };
 
+class cellInfo{
+readonly id : string;
+readonly expr: string;
+readonly ast : string;
+readonly value : number;
+readonly dependents : string;
+
+  constructor(id : string,expr: string,ast: string,value:number,dependents: string){
+    this.id = id;
+    this.expr = expr;
+    this.ast = ast;
+    this.value = value;
+    this.dependents = dependents;
+
+  }
+  
+}
+
+
+// let cell:cellInfo[];
+
 export class Spreadsheet {
+
+  
 
   readonly name: string;
   //TODO: add other instance variable declarations  
   constructor(name: string) {
     this.name = name;
+    
     //TODO: add initializations for other instance variables
   }
 
@@ -32,11 +57,109 @@ export class Spreadsheet {
    *  to a suitable error message.
    */
   async eval(cellId: string, expr: string) : Promise<Result<Updates>> {
-    //TODO
-    return okResult({}); //initial dummy result
+
+    // let dict ={}
+    let keyname : string = cellId;
+    JSON.stringify(keyname)
+    let stringToObject = {
+      [keyname] : expr
+    }
+    console.log()
+    //TODO  
+    // console.log(cellId);  
+    // console.log(expr);
+     let value = parse(expr);
+    //  console.log(value)
+    //  console.log(cellId)
+    //  console.log(expr)
+    // let value = this.Avalue(ast.val);
+    console.log(JSON.stringify(value,null,2));
+
+    let res = 0;
+    if(value.isOk)
+    {
+      // console.log(value.val);
+      res = this.Avalue(value.val);
+      
+    }
+    
+    
+    // console.log(value);
+    return okResult({[keyname]: res }); //initial dummy result
   }
     
   //TODO: add additional methods
+
+  /*
+   Avalue(ast: Ast): number {
+    if(ast.kind == "num")
+    {
+      var arr = [];
+      if(arr.length < 2){
+      arr.push(ast.value);
+      }
+     return ast.value;
+    }
+    if(ast.kind == "app")
+    {
+
+      console.log(FNS[ast.fn](this.Avalue(ast.kids[0]),this.Avalue(ast.kids[1])))
+      // return this.Avalue(ast.kids[0]) + this.Avalue(ast.kids[1]);
+      return FNS[ast.fn](this.Avalue(ast.kids[0]),this.Avalue(ast.kids[1]));
+      
+      
+      
+    }
+
+    
+
+    return 0;
+  }
+}
+*/
+
+/*
+Avalue(ast: Ast): number {
+  if (ast.kind === "num") {
+    return ast.value;
+    
+  } else if (ast.kind === "app") {
+    const fn = FNS[ast.fn];
+    const args = ast.kids.map((kid) => this.Avalue(kid));
+    return fn.apply(null,args);
+  }
+  return 0;
+}
+}
+*/
+
+ Avalue(node: Ast): number {
+  if (node.kind === 'num') {
+    return node.value;
+  } else if (node.kind === 'app') {
+     const kidValues = node.kids.map((kid) => this.Avalue(kid));
+     
+      console.log(kidValues)
+     
+     if(node.fn)
+     {
+         console.log(node.fn)
+         if(kidValues.length == 2)
+         {
+             return FNS[node.fn](kidValues[0],kidValues[1]);
+         }
+         else if(node.fn == '-')
+         {
+             return (-1 * kidValues[0]);
+         }
+         else {
+             return kidValues[0];
+         }
+     }
+    
+  }
+  return 0;
+}
 }
 
 //TODO: add additional classes and/or functions
