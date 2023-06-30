263aa5dfe239e5f7d9c56b388b43409b3a2662b6
Rujuta Vivek Vaidya <rvaidya2@binghamton.edu>
2023-06-30 13:17:43 -0400

    Added test case to check if clear spreadsheet functionality works

--- submit/prj2-sol/src/test/spreadsheet-dao.ts
+++ submit/prj2-sol/src/test/spreadsheet-dao.ts
@@ -1,15 +1,13 @@
 //will run the project DAO using an in-memory mongodb server
-import MemSpreadsheetDao from './spreadsheet-mem-dao.js';
+import MemSpreadsheetDao from "./spreadsheet-mem-dao.js";
 
-import { makeSpreadsheetDao, SpreadsheetDao } from '../lib/spreadsheet-dao.js';
+import { makeSpreadsheetDao, SpreadsheetDao } from "../lib/spreadsheet-dao.js";
 
+import { assert, expect } from "chai";
 
-import { assert, expect } from 'chai';
+const TEST_SPREADSHEET_NAME = "test";
 
-const TEST_SPREADSHEET_NAME = 'test';
-
-describe('spreadsheet DAO', () => {
- 
+describe("spreadsheet DAO", () => {
   let dao: SpreadsheetDao;
 
   beforeEach(async () => {
@@ -20,19 +18,19 @@ describe('spreadsheet DAO', () => {
     await MemSpreadsheetDao.tearDown(dao);
   });
 
-  it('must return spreadsheet name', () => {
+  it("must return spreadsheet name", () => {
     expect(dao.getSpreadsheetName()).to.equal(TEST_SPREADSHEET_NAME);
   });
 
-  it('must query an empty cell', async () => {
-    const cellId = 'x2';
+  it("must query an empty cell", async () => {
+    const cellId = "x2";
     const queryResult = await dao.query(cellId);
     assert(queryResult.isOk === true);
-    expect(queryResult.val).to.equal('');
+    expect(queryResult.val).to.equal("");
   });
 
-  it('must set cell expression', async () => {
-    const [cellId, expr] = ['a1', 'a2 * 3'];
+  it("must set cell expression", async () => {
+    const [cellId, expr] = ["a1", "a2 * 3"];
     const setResult = await dao.setCellExpr(cellId, expr);
     assert(setResult.isOk === true);
     const queryResult = await dao.query(cellId);
@@ -40,13 +38,12 @@ describe('spreadsheet DAO', () => {
     expect(queryResult.val).to.equal(expr);
   });
 
-  
-  it('must set multiple cell expressions', async () => {
+  it("must set multiple cell expressions", async () => {
     const cellExprs = [
-      ['a1', 'a2 * 3'],
-      ['a2', 'a3 + 4'],
-      ['a3', 'd5 / 6'],
-      ['d5', '42' ],
+      ["a1", "a2 * 3"],
+      ["a2", "a3 + 4"],
+      ["a3", "d5 / 6"],
+      ["d5", "42"],
     ];
     for (const [cellId, expr] of cellExprs) {
       const setResult = await dao.setCellExpr(cellId, expr);
@@ -59,22 +56,46 @@ describe('spreadsheet DAO', () => {
     }
   });
 
-  it('must clear spreadsheet', async () => {
+  it("must clear spreadsheet", async () => {
     //TODO
-  });
-  
-  it('must remove cells from spreadsheet', async () => {
     const cellExprs = [
-      ['a1', 'a2 * 3'],
-      ['a2', 'a3 + 4'],
-      ['a3', 'd5 / 6'],
-      ['d5', '42' ],
+      ["a1", "5"],
+      ["b2", "2 * a1"],
+      ["c3", "b2 + 3"],
+      ["d4", "c3 - b2"],
+    ];
+  
+    
+    for (const [cellId, expr] of cellExprs) {
+      const setResult = await dao.setCellExpr(cellId, expr);
+      assert(setResult.isOk === true);
+    }
+  
+  
+    const clearResult = await dao.clear();
+    assert(clearResult.isOk === true);
+  
+    
+    for (const [cellId, expr] of cellExprs) {
+      const queryResult = await dao.query(cellId);
+      assert(queryResult.isOk === true);
+      expect(queryResult.val).to.equal("");
+    }
+    
+  });
+
+  it("must remove cells from spreadsheet", async () => {
+    const cellExprs = [
+      ["a1", "a2 * 3"],
+      ["a2", "a3 + 4"],
+      ["a3", "d5 / 6"],
+      ["d5", "42"],
     ];
     for (const [cellId, expr] of cellExprs) {
       const setResult = await dao.setCellExpr(cellId, expr);
       assert(setResult.isOk === true);
     }
-    const rmCellIds = [ 'a1', 'd5', 'a8' ];
+    const rmCellIds = ["a1", "d5", "a8"];
     for (const cellId of rmCellIds) {
       const rmResult = await dao.remove(cellId);
       assert(rmResult.isOk === true);
@@ -82,23 +103,23 @@ describe('spreadsheet DAO', () => {
     for (const [cellId, expr] of cellExprs) {
       const queryResult = await dao.query(cellId);
       assert(queryResult.isOk === true);
-      const expected = rmCellIds.includes(cellId) ? '' : expr;
+      const expected = rmCellIds.includes(cellId) ? "" : expr;
       expect(queryResult.val).to.equal(expected);
     }
   });
 
-  it('must get data from empty spreadsheet', async () => {
+  it("must get data from empty spreadsheet", async () => {
     const dataResult = await dao.getData();
     assert(dataResult.isOk === true);
     expect(dataResult.val).to.deep.equal([]);
   });
 
-  it('must get data from spreadsheet', async () => {
+  it("must get data from spreadsheet", async () => {
     const cellExprs = [
-      ['a1', 'a2 * 3'],
-      ['a2', 'a3 + 4'],
-      ['a3', 'd5 / 6'],
-      ['d5', '42' ],
+      ["a1", "a2 * 3"],
+      ["a2", "a3 + 4"],
+      ["a3", "d5 / 6"],
+      ["d5", "42"],
     ];
     for (const [cellId, expr] of cellExprs) {
       const setResult = await dao.setCellExpr(cellId, expr);
@@ -106,17 +127,14 @@ describe('spreadsheet DAO', () => {
     }
     const dataResult = await dao.getData();
     assert(dataResult.isOk === true);
-    const cellSort =
-      (a: [string, string], b: [string, string]) => a[0].localeCompare(b[0]);
+    const cellSort = (a: [string, string], b: [string, string]) =>
+      a[0].localeCompare(b[0]);
     expect(dataResult.val.sort(cellSort)).to.deep.equal(cellExprs);
   });
-
 });
 
-
-describe('verify actual spreadsheet dao persistence', () => {
-
-  const dbUrl = 'mongodb://localhost:27017/spreadsheets';
+describe("verify actual spreadsheet dao persistence", () => {
+  const dbUrl = "mongodb://localhost:27017/spreadsheets";
 
   async function makeDao() {
     const makeResult = await makeSpreadsheetDao(dbUrl, TEST_SPREADSHEET_NAME);
@@ -132,9 +150,9 @@ describe('verify actual spreadsheet dao persistence', () => {
     assert(closeResult.isOk === true);
   });
 
-  it('must set cell expression', async () => {
+  it("must set cell expression", async () => {
     const dao1 = await makeDao();
-    const [cellId, expr] = ['a1', 'a2 * 3'];
+    const [cellId, expr] = ["a1", "a2 * 3"];
     const setResult = await dao1.setCellExpr(cellId, expr);
     assert(setResult.isOk === true);
     const closeResult1 = await dao1.close();
@@ -148,13 +166,12 @@ describe('verify actual spreadsheet dao persistence', () => {
     assert(closeResult2.isOk === true);
   });
 
-  
-  it('must set multiple cell expressions', async () => {
+  it("must set multiple cell expressions", async () => {
     const cellExprs = [
-      ['a1', 'a2 * 3'],
-      ['a2', 'a3 + 4'],
-      ['a3', 'd5 / 6'],
-      ['d5', '42' ],
+      ["a1", "a2 * 3"],
+      ["a2", "a3 + 4"],
+      ["a3", "d5 / 6"],
+      ["d5", "42"],
     ];
     const dao1 = await makeDao();
     for (const [cellId, expr] of cellExprs) {
@@ -173,16 +190,13 @@ describe('verify actual spreadsheet dao persistence', () => {
     const closeResult2 = await dao2.close();
     assert(closeResult2.isOk === true);
   });
-  
 });
 
-describe('must catch bad spreadsheet creation', () => {
-
-  it('must catch badly formed db url', async () => {
-    const daoResult = await makeSpreadsheetDao('localhost:27017/', 'test');
+describe("must catch bad spreadsheet creation", () => {
+  it("must catch badly formed db url", async () => {
+    const daoResult = await makeSpreadsheetDao("localhost:27017/", "test");
     assert(daoResult.isOk === false);
     expect(daoResult.errors.length).to.be.above(0);
-    expect(daoResult.errors[0].options.code).to.equal('DB');
+    expect(daoResult.errors[0].options.code).to.equal("DB");
   });
-  
 });
