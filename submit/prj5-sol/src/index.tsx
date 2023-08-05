e4252ca943af55c37e7b85a05e9f65eebf9232b2
Rujuta Vivek Vaidya <rvaidya2@binghamton.edu>
2023-08-05 12:17:10 -0400

    Added component files

--- submit/prj5-sol/src/index.tsx
+++ submit/prj5-sol/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-
+const WS_URL = 'https://zdu.binghamton.edu:2345';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
---
+++ submit/prj5-sol/src/spreadsheet.ts
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import SpreadsheetWs from './ss-ws.js';
+import { Errors, makeElement } from './utils';
+
+const N_ROWS = 10;
+const N_COLS = 10;
+
